0ff12830b79feb3944ef58b93fc96a470105359a
ykhadilkar <ykhadilkar@pivotal.io>
2019-04-26 20:42:25 -0400

    adding variables

--- terraforming-pas/variables.tf
+++ terraforming-pas/variables.tf
@@ -124,6 +124,23 @@ variable "ssl_ca_private_key" {
   default     = ""
 }
 
+variable "https_web_cert" {
+  type        = "string"
+  description = "the contents of an SSL certificate to be used by the LB, optional if `ssl_ca_cert` is provided"
+  default     = ""
+}
+
+variable "https_web_key" {
+  type        = "string"
+  description = "the contents of an SSL certificate to be used by the LB, optional if `ssl_ca_cert` is provided"
+  default     = ""
+}
+
+variable "https_web_chain" {
+  type        = "string"
+  description = "the contents of an SSL certificate to be used by the LB, optional if `ssl_ca_cert` is provided"
+  default     = ""
+}
 /*****************************
  * Isolation Segment Options *
  *****************************/
